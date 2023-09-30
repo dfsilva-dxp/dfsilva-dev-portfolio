80dbf43d11c971a926c8cd5ee455922b745fc673
Daniel Silva <dfsilva.dxp@gmail.com>
2023-09-30 10:18:04 -0300

    nit: commit do boilarplate inicial.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,20 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import Head from 'next/head';
+import { Roboto } from 'next/font/google';
 
 import { Providers } from './providers';
 
 import StyledComponentsRegistry from '@/lib/registry';
 
-const inter = Inter({ subsets: ['latin'] });
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap'
+});
 
 export const metadata: Metadata = {
-  title: 'Next Boilerplate',
-  description:
-    'Boilerplate generated with create next app and my personal configuration'
+  title: 'Daniel Silva • Sênior Front-End Developer',
+  description: 'Desenvolver se tornou um hobby favorito!'
 };
 
 export default function RootLayout({
@@ -19,8 +23,33 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="pt-BR">
+      <Head>
+        <title>Daniel Silva &bull; Sênior Front-End Developer</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <link rel="manifest" href="/manifest.json" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://dfsilva.dev" />
+
+        <meta
+          name="keywords"
+          content="front-end, desenvolvedor front-end, front end, daniel silva front end, dfsilva front end, daniel silva, dfsilva, dev, dev front-end, desenvolver sites, programação de sites, portfolio"
+        />
+
+        <link
+          rel="shortcut icon"
+          href="/img/icon/icon-512.png"
+          type="image/x-icon"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/img/icon/icon-512.png"
+          type="image/x-icon"
+        />
+      </Head>
+      <body className={roboto.className}>
         <StyledComponentsRegistry>
           <Providers>{children}</Providers>
         </StyledComponentsRegistry>
---
+++ src/styles/theme/index.ts
@@ -0,0 +1,68 @@
+'use client';
+
+const defaultTheme = {
+  grid: {
+    container: '130rem',
+    gutter: '3.2rem'
+  },
+  border: {
+    radius: '0.4rem'
+  },
+  font: {
+    family:
+      "Roboto, -apple-system, BlinkMacSystemFont, 'Segoe UI', Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    weight: {
+      regular: 300,
+      medium: 400,
+      semibold: 500,
+      bold: 700
+    },
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '2.4rem',
+      xlarge: '3.6rem',
+      xxlarge: '6rem',
+      huge: '22rem'
+    }
+  },
+  colors: {
+    white: '#FFF',
+    primary: {
+      main: '#234B7A',
+      dark: '#040506'
+    },
+    purple: {
+      main: '#3A3051'
+    },
+    gray: {
+      50: '#FAFAFA',
+      100: '#F2F3F5',
+      200: '#C7CFD9',
+      300: '#AFBFD6',
+      400: '#8D95AF',
+      700: '#212A36',
+      800: '#0E1116'
+    },
+    green: {
+      main: '#43F0C7'
+    },
+    red: {
+      main: '#FF6347'
+    }
+  },
+  layers: {
+    base: 10,
+    menu: 20,
+    overlay: 30,
+    modal: 40,
+    alwaysOnTop: 50
+  },
+  transition: {
+    default: '0.3s ease-in-out',
+    fast: '0.1s ease-in-out'
+  }
+} as const;
+
+export default defaultTheme;
