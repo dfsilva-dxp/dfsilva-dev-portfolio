7820bc39d0596401596a8ae85908ece2d543f9eb
Daniel Silva <dfsilva.dxp@gmail.com>
2023-10-05 17:08:11 -0300

    refactor: refatora o componente flex.

--- src/components/Flex/styles.ts
+++ src/components/Flex/styles.ts
@@ -2,25 +2,9 @@
 
 import styled, { css } from 'styled-components';
 
-import { IFlexProps } from '.';
+import { FlexType } from '.';
 
-type FlexContentType = {
-  gap:
-    | '0'
-    | '0.4rem'
-    | '0.8rem'
-    | '1rem'
-    | '1.6rem'
-    | '2rem'
-    | '2.4rem'
-    | '2.8rem'
-    | '3rem'
-    | '3.2rem'
-    | '3.4rem'
-    | '3.8rem';
-} & Omit<IFlexProps, 'children' | 'gap'>;
-
-export const FlexContent = styled.div<FlexContentType>`
+export const FlexContent = styled.div<FlexType>`
   ${({ display, direction, align, justify, content, wrap, gap }) => css`
     display: ${display};
     flex-direction: ${direction};
