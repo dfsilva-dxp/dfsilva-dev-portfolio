9d1641a32ba5105f0b3ffc9e6356b215fd2a1514
Daniel Silva <dfsilva.dxp@gmail.com>
2023-10-06 08:48:35 -0300

    fix: corrige o erro do next-route-announcer.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,73 +4,50 @@ import { AnimatePresence, motion } from 'framer-motion';
 import { Container, Flex } from '@/components';
 
 import * as S from './styles';
-
-const menuVars = {
-  initial: {
-    scaleY: 0
-  },
-  animate: {
-    scaleY: 1,
-    transition: {
-      duration: 0.5,
-      ease: [0.12, 0, 0.39, 0]
-    }
-  },
-  exit: {
-    scaleY: 0,
-    transition: {
-      delay: 0.5,
-      duration: 0.5,
-      ease: [0.22, 1, 0.36, 1]
-    }
-  }
-};
-const containerVars = {
-  initial: {
-    opacity: 0,
-    transition: {
-      staggerChildren: 0.09,
-      staggerDirection: -1
-    }
-  },
-  open: {
-    opacity: 1,
-    transition: {
-      ease: [0.12, 0, 0.39, 0],
-      duration: 0.5,
-      staggerDirection: -1,
-      delay: 0.5
-    }
-  }
-};
+import {
+  content,
+  fullscreen,
+  header,
+  navButtonAnimation
+} from './motion-config';
 
 const Header = () => {
   const [isActive, setIsActive] = useState(false);
 
   const toggleMenu = () => {
-    setTimeout(() => setIsActive((current) => !current), 500);
+    setTimeout(() => setIsActive((current) => !current), 300);
   };
 
   return (
     <>
-      <S.HeaderContent>
-        <Flex align="center" justify="flex-end">
-          <S.ToggleButton type="button" onClick={toggleMenu}>
-            {isActive ? 'Voltar' : 'Navegue'}
-          </S.ToggleButton>
-        </Flex>
-      </S.HeaderContent>
+      <motion.div
+        variants={header}
+        initial="initial"
+        animate="animate"
+        className="motion-container"
+      >
+        <S.HeaderContent>
+          <Flex align="center" justify="flex-end">
+            <motion.div variants={navButtonAnimation}>
+              <S.ToggleButton type="button" onClick={toggleMenu}>
+                {isActive ? 'Voltar' : 'Navegue'}
+              </S.ToggleButton>
+            </motion.div>
+          </Flex>
+        </S.HeaderContent>
+      </motion.div>
+
       <AnimatePresence>
         {isActive && (
           <S.Overlay
-            variants={menuVars}
+            variants={fullscreen}
             initial="initial"
             animate="animate"
             exit="exit"
           >
             <Container>
               <motion.div
-                variants={containerVars}
+                variants={content}
                 initial="initial"
                 animate="open"
                 exit="initial"
