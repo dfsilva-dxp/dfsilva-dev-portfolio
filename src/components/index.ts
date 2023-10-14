082ba974b72f6ec1cd08a31194d44b5ea4af9060
Daniel Silva <dfsilva.dxp@gmail.com>
2023-10-14 12:47:25 -0300

    nit: add boilerplate.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,1 @@
-export { default as CodeLine } from './CodeLine';
-export { default as Container } from './Container';
 export { default as Flex } from './Flex';
-export { default as Header } from './Header';
-export { default as HelloWorld } from './HelloWorld';
-export { default as Hero } from './Hero';
-export { default as Loader } from './Loader';
-export { default as Tag } from './Tag';
